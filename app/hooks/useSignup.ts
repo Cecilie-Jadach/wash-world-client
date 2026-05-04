a93afeb28d4c6daeeb9d15fcd5a966e3b82c184f
Line <lith0001@stud.kea.dk>
2026-05-04 15:42:18 +0200

    created useSignup hook

---
+++ app/hooks/useSignup.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export const useSignUp = () => {
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const signUp = async (formData: FormData) => {
+    setIsLoading(true);
+    setError("");
+    setMessage("");
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BASE_URL + "/sign-up",
+        {
+          method: "POST",
+          body: formData,
+        },
+      );
+
+      if (!response.ok) {
+        const data = await response.json();
+        setError(data.error);
+        return;
+      }
+
+      setMessage("Bruger oprettet! Tjek din email.");
+    } catch (error) {
+      setError("Error connecting to backend");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { signUp, message, isLoading, error };
+};
