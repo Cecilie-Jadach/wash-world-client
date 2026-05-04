8ec0c790ffa9c109b93cae4ff7190bd94fbd2859
Line <lith0001@stud.kea.dk>
2026-05-04 10:41:17 +0200

    input created

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { cva } from 'class-variance-authority'
-import { VariantProps } from 'class-variance-authority'
+import React from 'react';
+import { cva } from 'class-variance-authority';
+import { VariantProps } from 'class-variance-authority';
 
 type ButtonProps = VariantProps<typeof buttonStyles> & {
     children: React.ReactNode
