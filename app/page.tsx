e001aefc0517b236fcc83a6de11a59a89d3f3378
Cecilie Jadach Petersen <Jadach95@hotmail.com>
2026-05-04 12:53:26 +0200

    removed initial styling and content in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Button from "@/components/Button"
-import Input from "@/components/Input";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
+    <div>
+      <main>
+        
       </main>
     </div>
   );
