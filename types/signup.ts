f26a0983a1ccc0fbc41705fba157b30b301b5bac
Line <lith0001@stud.kea.dk>
2026-05-07 14:49:33 +0200

    feat: add SignupFormData type for user registration

---
+++ types/signup.ts
@@ -0,0 +1,10 @@
+export type SignupFormData = {
+  email: string;
+  password: string;
+  phone: string;
+  license_plate: string;
+  primary_location: string;
+  terms_accepted: boolean;
+  access_to_all_washes: boolean;
+  offers_accepted: boolean;
+};
