94eb72e8985a9fced618c53df5c04cdd767e73d9
Line <lith0001@stud.kea.dk>
2026-05-11 15:38:26 +0200

    feat: update Navbar component to hide on signup, login, and verification pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     const [activeSupport, setActiveSupport] = useState(false);
     const isActive = (path: string) => pathname === path ? 'text-black' : 'text-grey-60'
 
-    if (pathname === "/signup") return null
+    if (["/signup", "/login"].includes(pathname) || pathname.startsWith("/verify")) return null
 
     return (
         <>
