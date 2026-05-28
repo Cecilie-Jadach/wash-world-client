1b3818def6dfccec35e2b47c45c0ace4b8a3fe0f
Cecilie Jadach Petersen <Jadach95@hotmail.com>
2026-05-28 14:13:31 +0200

    fix: improve error handling messages in login and toggle notification components

--- components/ToggleNotification.tsx
+++ components/ToggleNotification.tsx
@@ -1,38 +1,20 @@
 import { useToggleOffers } from "@/app/hooks/useToggleOffers";
-import { useState, useEffect } from "react";
+
 
 export default function ToggleNotification() {
-    //Henter to ting fra useToggleOffers hook
-    //offersData er brugerens nuværende data fra backend, 
-    //og toggleMutation er funktionen der kalder PATCH endpointet.
     const { toggleMutation, offersData } = useToggleOffers();
-    //Opretter en lokal state der styrer om togglen er tændt eller slukket. 
-    // Den starter som false fordi vi endnu ikke ved hvad værdien er fra backend.
-    const [isToggled, setIsToggled] = useState(false);
 
-    //Denne funktion kører hver gang offersData ændrer sig. 
-    //Det sker fx når siden loader og React Query henter data fra backend. 
-    //Uden denne ville togglen altid starte som false uanset hvad der er gemt i databasen.
-    useEffect(() => {
-        if (offersData) {
-        //Sætter isToggled til true hvis backend returnerer 1, og false hvis den returnerer 0. 
-        //Det er her databaseværdien oversættes til en boolean.
-            setIsToggled(offersData.offers_accepted === 1);
-        }
-    }, [offersData]);
+    const isToggled = offersData?.offers_accepted === 1;
 
-    //Kaldes når brugeren klikker. 
-    //Den gør to ting på én gang — setIsToggled(prev => !prev) flipper den visuelle toggle med det samme så det føles responsivt, 
-    //og toggleMutation.mutate() sender PATCH kaldet til backend i baggrunden.
     const handleToggle = () => {
-    setIsToggled(prev => !prev); // Optimistisk opdatering
-    //undefined fordi vi ikke sender nogen data til mutationFn, men det er nødvendigt at have det spm argument for at komme ned til onError
-    toggleMutation.mutate(undefined, {
-        onError: () => {
-            setIsToggled(prev => !prev); // Rul tilbage ved fejl
-        }
-    });
-}
+        toggleMutation.mutate(undefined, {
+            onError: () => {
+                // React Query genindlæser automatisk via invalidateQueries
+            }
+        });
+    }
+
+    console.log("offersData:", offersData)
 
     return (
         <label htmlFor="notification-toggle" className={`flex cursor-pointer relative w-lg h-sm rounded-full ${isToggled ? `bg-green-white-background` : `bg-grey-60`} ` }>
